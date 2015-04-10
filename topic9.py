7f29a3c0e6e652a81c813578adcdf23a48c0cd0e
Blue Waters Trainee 834 <tra834@h2ologin3.ncsa.illinois.edu>
2015-04-10 16:31:15 -0500

    Add readme

--- topic9.py
+++ topic9.py
@@ -3,6 +3,7 @@ import sys
 import numpy as np
 import math
 
+comm = MPI.COMM_WORLD
 size = MPI.COMM_WORLD.Get_size()
 rank = MPI.COMM_WORLD.Get_rank()
 name = MPI.Get_processor_name()
@@ -58,9 +59,21 @@ if rank == 0:
 
 				proc = 1+ z*ypartitions*xpartitions + y*xpartitions + x
 				sys.stdout.write("Subvolume <%d,%d> <%d,%d> <%d,%d> is assigned to process %d\n" % (xmin,xmax,ymin,ymax,zmin,zmax,proc))				
-# 2 + 2*3 + 2*3*3 = 2 + 6 + 18 = 26
+
+	
+				#comm.Send([[xmin,xmax,ymin,ymax,zmin,zmax],MPI.INT],dest=proc,tag=77)
+				toSend = np.array([xmin,xmax,ymin,ymax,zmin,zmax])
+				comm.Send([toSend,MPI.INT],dest=proc,tag=77)
+# 2 + 2*3 + 2*3*3 = 2 + 6 + 18 = `26
+
+else:
+	comm.Recv([data,MPI.INT],dest=proc,tag=77)
+	[xmin,xmax,ymin,ymax,zmin,zmax] = np.asarray(data)
+	sys.stdout.write("Process %d received %s\n" %(rank, [xmin,xmax,ymin,ymax,zmin,zmax]))
+
+comm.Barrier()
+data = comm.bcast(data, root=0)
+comm.Barrier()
 
 
-
-
-
+sys.stdout.write("Process %d: %s\n" % (rank, str(data.shape())))
